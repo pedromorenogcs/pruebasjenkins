6f90a3514cc9304a70c37071ea74eb8dae282d42
Pedro Moreno <pmoreno@MacBook-Air-de-Pedro.local>
2024-09-04 18:52:56 +0200

    date

--- web/index.php
+++ web/index.php
@@ -2,9 +2,9 @@
 <body bgColor=white>
 <h1>Entraaaa</h1>
 <?php
-echo 'USER: ' . php_uname() . chr(10);
-echo gethostname() . chr(10);
-echo PHP_OS . chr(10);
+echo 'USER: ' . php_uname() . '<br/>';
+echo gethostname() . . '<br/>';
+echo PHP_OS .  '<br/>';
 ?>
 </body>
 </html>
