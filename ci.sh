4552655005a1d56da6ac0bab9327ce8d8e00278e
Pedro Moreno <pmoreno@MacBook-Air-de-Pedro.local>
2024-10-02 09:34:15 +0200

    commit

--- ci.sh
+++ ci.sh
@@ -2,4 +2,4 @@
 git add .
 git commit -m "commit"
 git push
-#curl -X POST "http://pmoreno:11a689a52b07400d59883f3be91790a01c@192.168.33.10:8080/job/pipelineAnsible/build"
+curl -X POST "http://pmoreno:11ba538babc21e3cd99e5295e2b55faf87@192.168.33.10:8080/job/pipelineAnsible/build"
