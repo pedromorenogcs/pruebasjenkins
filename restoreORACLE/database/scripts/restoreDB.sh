85d9f742a55a9ee0285b8ccef50df40098a094f0
Pedro Moreno <pmoreno@MacBook-Air-de-Pedro.local>
2025-01-08 11:02:38 +0100

    commit

--- restoreORACLE/database/scripts/restoreDB.sh
+++ restoreORACLE/database/scripts/restoreDB.sh
@@ -1,4 +1,5 @@
-export ORACLE_HOME=/u01/app/oracle/product/19.0.0/dbhome_1
+#export ORACLE_HOME=/u01/app/oracle/product/19.0.0/dbhome_1
+export ORACLE_HOME=/u01/app/oracle/product/19.3.0/dbhome_1
 export PATH=$ORACLE_HOME/bin:$PATH
 export ORACLE_SID={{oracle_sid}}
 export ORACLE_SID=COREP_DR
