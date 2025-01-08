d0d50102e23eca2ce55ac8e09569ef65489b8888
Pedro Moreno <pmoreno@MacBook-Air-de-Pedro.local>
2025-01-08 16:02:06 +0100

    commit

--- restoreORACLE/database/scripts/restoreDB.sh
+++ restoreORACLE/database/scripts/restoreDB.sh
@@ -1,6 +1,7 @@
 #export ORACLE_HOME=/u01/app/oracle/product/19.0.0/dbhome_1
 export ORACLE_HOME=/u01/app/oracle/product/19.3.0/dbhome_1
 export PATH=$ORACLE_HOME/bin:$PATH
+export NLS_DATE_FORMAT="dd-mm-yy hh24:mi:ss"
 export ORACLE_SID={{oracle_sid}}
 #export ORACLE_SID=COREP_DR
 echo $ORACLE_SID
