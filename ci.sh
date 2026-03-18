ed200a59cc7b9053c85953cec4237d72ac03c634
Pedro Moreno <pmoreno@MacBook-Air-de-Pedro.local>
2026-03-18 10:07:04 +0100

    commit

--- ci.sh
+++ ci.sh
@@ -2,6 +2,6 @@
 git pull
 git add .
 git commit -m "commit"
-git push
+GIT_SSH_COMMAND='ssh -i $HOME/.ssh/pruebasM2MGitHub -o IdentitiesOnly=yes' git push
 #curl -X POST "http://pmoreno:11ba538babc21e3cd99e5295e2b55faf87@192.168.33.10:8080/job/pipelineAnsible/build"
 #curl -X POST "http://pmoreno:11e282da32a3b34dedb7615d650da5a70a@192.168.33.10:8080/job/pipelinePrueba/build"
